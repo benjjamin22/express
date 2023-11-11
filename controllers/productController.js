4a6b26271a3429968b351c5eb4671ae2d6d3dea6
benjamin <deeprising4ilife@gmail.com>
2023-11-10 16:52:26 -0800

    first commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,9 +7,13 @@ const { getPostData } = require('../utils')
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
-
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(products))
+        if (!products) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found' }))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(products))
+        }
     } catch (error) {
         console.log(error)
     }
@@ -21,7 +25,7 @@ async function getProduct(req, res, id) {
     try {
         const product = await Product.findById(id)
 
-        if(!product) {
+        if (!product) {
             res.writeHead(404, { 'Content-Type': 'application/json' })
             res.end(JSON.stringify({ message: 'Product Not Found' }))
         } else {
@@ -50,7 +54,7 @@ async function createProduct(req, res) {
         const newProduct = await Product.create(product)
 
         res.writeHead(201, { 'Content-Type': 'application/json' })
-        return res.end(JSON.stringify(newProduct))  
+        return res.end(JSON.stringify(newProduct))
 
     } catch (error) {
         console.log(error)
@@ -63,7 +67,7 @@ async function updateProduct(req, res, id) {
     try {
         const product = await Product.findById(id)
 
-        if(!product) {
+        if (!product) {
             res.writeHead(404, { 'Content-Type': 'application/json' })
             res.end(JSON.stringify({ message: 'Product Not Found' }))
         } else {
@@ -80,9 +84,9 @@ async function updateProduct(req, res, id) {
             const updProduct = await Product.update(id, productData)
 
             res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify(updProduct)) 
+            return res.end(JSON.stringify(updProduct))
         }
- 
+
 
     } catch (error) {
         console.log(error)
@@ -95,7 +99,7 @@ async function deleteProduct(req, res, id) {
     try {
         const product = await Product.findById(id)
 
-        if(!product) {
+        if (!product) {
             res.writeHead(404, { 'Content-Type': 'application/json' })
             res.end(JSON.stringify({ message: 'Product Not Found' }))
         } else {
